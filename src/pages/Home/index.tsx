b2445110d04b2c74f509b9b63f49372b275315a1
Wesley de Andrade Santos <wesleyandrade298@gmail.com>
2021-03-21 17:30:50 -0300

    Creating Routes

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.scss'
+
+const Home = () =>{
+
+    return(
+        <h1>Home</h1>
+    );
+}
+
+export default Home;
---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.scss'
+const Search = () =>{
+
+    return(
+        <h1>Localizar um usuário</h1>
+    );
+}
+
+export default Search;
